9327a5f2dc28dab88b48739292ce233dbf3d9277
Jason Cardinal <jason.brogrammer@gmail.com>
2017-01-04 16:06:47 -0600

    Milestone tracker

--- src/app/milestone-tracker/milestone-tracker.component.ts
+++ src/app/milestone-tracker/milestone-tracker.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, trigger, state, style, transition, animate } from '@angular/core';
 
 import { MdDialog, MdDialogRef, MdSnackBar } from '@angular/material';
 
@@ -10,14 +10,53 @@ import { FirebaseObjectObservable } from 'angularfire2';
 @Component({
   selector: 'app-milestone-tracker',
   templateUrl: './milestone-tracker.component.html',
-  styleUrls: ['./milestone-tracker.component.css']
+  styleUrls: ['./milestone-tracker.component.css'],
+  animations: [
+    trigger('milestoneFlyIn', [
+      state('void', style({ transform: 'translateY(50%)', opacity: 0 })),
+      state('in', style({ transform: 'translateY(0%)', opacity: 1 })),
+      state('out', style({ transform: 'translateY(50%)', opacity: 0 })),
+      transition("* => *", animate(`750ms 200ms ease`))
+    ]),
+  ]
 })
-export class MilestoneTrackerComponent implements OnInit {
+export class MilestoneTrackerComponent implements OnInit, AfterViewInit {
   products;
   constructor(private productLogos: ProductLogosService,private router: Router, private userInfo: UserInfoService) { }
 
+  getDate(product, milestone) {
+    if(product && milestone) {
+      return this.userInfo.getDayDiff(Date.parse(product.started), Date.parse(product.activation), milestone.days_differential);
+    }
+    return null;
+  }
+
+  getClass(status) {
+    switch(status) {
+      case 'In Progress':
+        return 'in-progress event';
+      case 'Needs Attention':
+        return 'needs-attention event'
+      case 'Complete':
+        return 'complete event'
+      default:
+        break;
+    }
+    return 'event';
+  }
+
   ngOnInit() {
     this.products = this.productLogos.getProductLogos().map(function (v) { return { logo_info: v } });
+  }
+
+  getDelay(i) {
+    return {
+      transitionDelay: (i + 1 * 0.5) + 's'
+    };
+  }
+
+  ngAfterViewInit() {
+
     var p, m;
     p = this.userInfo.products;
     m = this.userInfo.milestones;
@@ -31,6 +70,10 @@ export class MilestoneTrackerComponent implements OnInit {
             products[i].combineLatest(milestones[i]).subscribe(([product, milestone]) => {
               console.log('COMBINDED SINGLE', product, milestone, this.products);
               this.products[idx].completion_info = this.userInfo.calculateMilestoneCompletion(product, milestone);
+              this.products[idx].completion_info.subscribe(info => {
+                this.products[idx].milestone_state = "in";
+                console.log("AFTER VIEW INIT");
+              })
             });
           }
         }
--- src/app/pipes/phone_format.pipe.ts
+++ src/app/pipes/phone_format.pipe.ts
@@ -2,9 +2,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({name: 'phone_format'})
 export class PhoneFormatPipe implements PipeTransform {
-  transform(phone) {
-    if(phone !== null) {
+  transform(phone, extension) {
+    if(phone !== null && extension === true) {
       return phone.toString().replace(/(\d{3})(\d{3})(\d{4})(x?)(\d*)/, '($1) $2-$3 $4 $5').trim();
+    } else if(phone !== null && extension === false) {
+      return phone.toString().replace(/(\d{3})(\d{3})(\d{4})(x?)(\d*)/, '($1) $2-$3').trim();
     }
     return null;
   }
